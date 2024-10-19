0b9cdb03857dff611b2aa4d543f8b169b8f4b2bc
TuPM <tupm.citgroup@gmail.com>
2024-10-19 10:25:55 +0700

    add

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        posts: resolve(__dirname, 'public/posts.json')
+      }
+    }
+  }
 })
